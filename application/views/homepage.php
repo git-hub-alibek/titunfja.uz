b8f0a6f8ffe8b79c9977ae7426283afaf90b10ee
alibek <git-hub-alibek>
2018-05-20 14:55:08 +0500

    suret qostim

--- application/views/homepage.php
+++ application/views/homepage.php
@@ -26,18 +26,15 @@
 						<!-- Block2 -->
 					
 						<div class="block2">	
-							<div class="block2-img wrap-pic-w of-hidden pos-relative block2-labelnew">
+							<div class="block2-img wrap-pic-w of-hidden pos-relative ">
 							<img src="/uploads/<?php echo $i['img'];?>" alt="IMG-PRODUCT">
 
 								<div class="block2-overlay trans-0-4">
-									<a href="#" class="block2-btn-addwishlist hov-pointer trans-0-4">
-										<i class="icon-wishlist icon_heart_alt" aria-hidden="true"></i>
-										<i class="icon-wishlist icon_heart dis-none" aria-hidden="true"></i>
-									</a>
+									
 
 									<div class="block2-btn-addcart w-size1 trans-0-4">
 								
-										<!-- Button -->
+										<!-- Button --> 
 										<button class="flex-c-m size1 bg4 bo-rad-23 hov1 s-text1 trans-0-4">
 											SU'WRETTI TOLÍQ ASHÍW
 										</button>
